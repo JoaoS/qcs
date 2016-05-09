1708d8815f1af08ea7c01305510a2ffe55784a6d
Joao Subtil <joaosubtil@BigMac.local>
2016-05-09 15:35:15 +0100

    Latest changes, 2 functions validated, missing mealtimeinsulindose()

--- UnitTests/insulinCalcTest.java
+++ UnitTests/insulinCalcTest.java
@@ -1,27 +1,78 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+import server.insulinCalc;
 
 /**
  * Created by joaosubtil on 04/05/16.
  */
+
 public class insulinCalcTest {
 
     @Test
     public void testMealtimeInsulinDose() throws Exception {
+        /*
+            method mealtimeInsulinDose(...)
 
-        insulinCalc s = new server.insulΩinCalc();
-        System.out.println(s.mealtimeInsulinDose(1,1,1,1,1));
+            input: 60, 12, 200, 100, 25 ---> expected output: 14
+
+            input: 95, 10, 100, 120, 50 ---> expected output: 0
+
+            input: 120, 14, 170, 100, 60 ---> expected output: 8
+        */
+
+        insulinCalc s = new insulinCalc();
+        System.out.println("Teste1="+s.mealtimeInsulinDose( 60, 12, 200, 100, 25));
+
+        System.out.println("Teste2="+s.mealtimeInsulinDose(  95, 10, 100, 120, 50));
+
+        System.out.println("Teste3="+s.mealtimeInsulinDose(  120, 14, 170, 100, 60));
 
 
     }
 
-    @Test
+    @Test/*Works ok*/
     public void testBackgroundInsulinDose() throws Exception {
 
+            insulinCalc s = new insulinCalc();
+            System.out.println(s.backgroundInsulinDose(79));
+
     }
 
-    @Test
+    @Test/*fails last test*/
     public void testPersonalSensitivityToInsulin() throws Exception {
+    /*
+    *   input: 5, {0, 10}, {50, 50} ---> expected output: 50
+
+        input: 6, {2, 8}, {32, 83} ---> expected output: 66
+
+        input: 0, {1, 3, 10}, {33, 43, 70} ---> expected output: 30
+
+        input: 4, {1, 6, 8, 9}, {32, 61, 91, 88} ---> expected output: 53
+        */
+        insulinCalc s = new insulinCalc();
+        int array1 [] ={0,10};
+        int array2 [] ={50,50};
+        System.out.println("Teste1="+s.personalSensitivityToInsulin(5,array1,array2));
+
+        int array3 [] ={2,8};
+        int array4 [] ={32,83};
+        System.out.println("Teste2="+s.personalSensitivityToInsulin(6,array3,array4));
+
+        int array5 [] ={1,3,10};
+        int array6 [] ={33,43,70};
+        System.out.println("Teste3="+s.personalSensitivityToInsulin(0,array5,array6));
+
+        int array7 [] ={1,6,8,9};
+        int array8 [] ={32,61,91,88};
+        System.out.println("Teste4="+s.personalSensitivityToInsulin(4,array7,array8)); //todo, falha este teste
 
     }
 }
+
+
+
+
+
+
+
+
+
--- src/server/insulinCalc.java
+++ src/server/insulinCalc.java
@@ -2,37 +2,66 @@ package server;
 /**
  * Created by joaosubtil on 04/05/16.
  */
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 
 
-@WebService()
+@WebService(targetNamespace= " http://localhost:9000/insulinCalc")
 public class insulinCalc implements InsulinDoseCalculator {
 
     public insulinCalc() {
     }
-
+    /**
+     * Calculates the number of insulin units needed after one meal.
+     * <p>
+     * This method takes the amount of carbohydrate in a given meal, and returns
+     * the number of units of insulin needed after that meal. The returned
+     * number of units of insulin equals the carbohydrate dose plus the high
+     * blood sugar dose, which are computed as follows.
+     * <p>
+     * The carbohydrate dose equals the total grams of carbohydrates in the meal
+     * divided by the amount of carbohydrate disposed by one unit of insulin,
+     * corrected by taking into account the personal sensitivity to insulin.
+     * This dose equals <code>carbohydrateAmount / carbohydrateToInsulinRatio /
+     * personalSensitivity x 50</code>.
+     * <p>
+     * The high blood sugar dose equals the difference between the pre-meal
+     * blood sugar level and the target blood sugar level, divided by the
+     * personal sensitivity to insulin. This equals <code>(preMealBloodSugar -
+     * targetBloodSugar) / personalSensitivity</code>. The personal sensitivity
+     * may be estimated using <code>personalSensitivityToInsulin()</code>.
+     * <p>
+     * In the special case when the target blood sugar level is greater than the
+     * pre-meal blood sugar level, the return value of this method is zero (no
+     * insulin).
+     *
+     * @param carbohydrateAmount amount of carbohydrate in the meal, in grams
+     * @param carbohydrateToInsulinRatio carbohydrate grams disposed by one unit
+     * @param preMealBloodSugar pre-meal measured blood sugar level, in mg/dl
+     * @param targetBloodSugar prescribed target blood sugar level, in mg/dl
+     * @param personalSensitivity personal sensitivity to insulin
+     * @return the mealtime units of insulin needed, or -1 in case of error
+     */
     @Override  @WebMethod
     public int mealtimeInsulinDose(int carbohydrateAmount, int carbohydrateToInsulinRatio, int preMealBloodSugar, int targetBloodSugar, int personalSensitivity) {
 
     try {
         double carbohydrate_dose,high_blood_sugar_dose;
-
+        double total;
         if(targetBloodSugar > preMealBloodSugar){
             return 0;
         }
 
         else{
 
-            carbohydrate_dose = carbohydrateAmount / carbohydrateToInsulinRatio /personalSensitivity * 50;
+            carbohydrate_dose =(( carbohydrateAmount / carbohydrateToInsulinRatio) /personalSensitivity) * 50;
 
             high_blood_sugar_dose = (preMealBloodSugar - targetBloodSugar) / personalSensitivity;
-
         }
-        int last= (int)Math.round(carbohydrate_dose+high_blood_sugar_dose);
-
-        return last;
+        return (int)Math.round(carbohydrate_dose+high_blood_sugar_dose);
 
     }catch (Exception e){
         return -1;
@@ -46,7 +75,7 @@ public class insulinCalc implements InsulinDoseCalculator {
 
         int output=0;
         try {
-            output = (int) (Math.round((0.55 * bodyWeight) / 0.5));
+            output = (int) (Math.round((0.55 * bodyWeight) * 0.5));
         }catch (Exception e) {
             System.out.println("Exception:" + e);
         }
